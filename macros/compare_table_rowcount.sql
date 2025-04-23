e7f3545cfa3cf02a8623aa5247f9971b2aff4230
Chandu PJ <chandu.pj96@gmail.com>
2025-04-23 06:32:07 +0000

    commit

---
+++ macros/compare_table_rowcount.sql
@@ -0,0 +1,2 @@
+{{ default__compare_row_counts
+('DB.STAGING.emp','DB.STAGING.raw_customerdata','DB.STAGING.raw_orders')}}
--- snapshots/snap_customers_timestamp2.sql
+++ snapshots/snap_customers_timestamp2.sql
@@ -1,11 +1,13 @@
-{% snapshot  snap_customers_timestamp %}
+{% snapshot snap_customers_timestamp2 %}
 
 
 {{
     config(
       strategy='timestamp',
       unique_key='id',
-      updated_at='updated_at'
+      updated_at='updated_at',
+      invalidate_hard_deletes=True   
+
     )
 }}
 select * 
